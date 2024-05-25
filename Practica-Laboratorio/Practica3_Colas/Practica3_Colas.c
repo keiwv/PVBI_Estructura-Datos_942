8919ad851de82f3490d9e7457dd9c78d31b8cd66
keiwv <brayan.perez56@uabc.edu.mx>
2024-05-24 22:02:07 -0700

    Modified queues and gitignore

--- Practica-Laboratorio/Practica3_Colas/Practica3_Colas.c
+++ Practica-Laboratorio/Practica3_Colas/Practica3_Colas.c
@@ -2,151 +2,177 @@
 #include <stdlib.h>
 #include "../teemo.h"
 
+#define True 1
+#define False 0
+#define Bool int
+
 typedef struct _Data
 {
     int dato;
 } Data;
-
 typedef struct Nodo
 {
     Data data;
     struct Nodo *siguiente;
 } Nodo;
-
-int msge_menu();
-
-void addNode(Nodo **head, int dato);
-int pop(Nodo **head);
-void showList(Nodo **head);
+typedef struct head
+{
+    Nodo *head;
+    Nodo *ultimo;
+} head;
 
 Nodo *createNode(int dato);
-Nodo *createList();
+head *createHead(int dato);
+void enqueue(head *cabeza, int dato);
+void dequeue(head *cabeza);
+int getFirstData(head *cabeza);
+Bool isEmpty(head *cabeza);
+int sizeCola(head *cabeza);
+void cleanCola(head *cabeza);
+void printQueue(head *cabeza);
 
 int main()
 {
-    Nodo *head = createList();
-    int op;
-    int dato;
-    int c;
-    int limite = 0;
-    do
-    {
-        system("clear");
-        op = msge_menu();
-        switch (op)
-        {
-        case 1:
-            printf("Por favor, ingresa el dato: ");
-            scanf("%d", &dato);
-            addNode(&head, dato);
-            break;
-        case 2:
-            printf("El dato es: ");
-            dato = pop(&head);
-            printf("%d\n", dato);
-            break;
-        case 3:
-            showList(&head);
-            break;
-        }
-        while ((c = getchar()) != '\n' && c != EOF) // clear buffer
-            ;
-        getchar();
-    } while (op != 0);
+    head *queue = createHead(2);
+
+    enqueue(queue, 10);
+    enqueue(queue, 20);
+    enqueue(queue, 30);
+
+    system("clear");
+
+    printQueue(queue);
+    printf("\n");
+
+    printf("Primer elemento: %d\n", getFirstData(queue));
+    printf("\n");
+
+    printf("Tamanio de la cola: %d\n", sizeCola(queue));
+    printf("\n");
+
+    dequeue(queue);
+    printf("2 ha sido sacado de la cola\n");
+    printQueue(queue);
+    printf("\n");
+
+    printf("La cola esta vacia: %s\n", isEmpty(queue) ? "verdadero" : "falso");
+    printf("\n");
+
+    cleanCola(queue);
+    printf("\n");
+
+    printQueue(queue);
+    printf("\n");
+
+    printf("La cola esta vacia: %s\n", isEmpty(queue) ? "verdadero" : "falso");
+    printf("\n");
+
+    free(queue);
 
     return 0;
 }
 
-int msge_menu()
-{
-    printf("---- M E N U -----\n");
-    printf("1.- Ingresar dato\n");
-    printf("2.- Leer dato\n");
-    printf("3.- Mostrar cola\n");
-    printf("0.- Salir\n");
-    return valid("Selecciona tu opción: ", 0, 3);
-}
-
 Nodo *createNode(int dato)
 {
-    Nodo *newNode = (Nodo *)malloc(sizeof(Nodo));
-
-    if (newNode == NULL)
-    {
-        printf("Missing memory\n");
-        exit(1);
-    }
-    newNode->data.dato = dato;
-    newNode->siguiente = NULL;
-    return newNode;
+    Nodo *temp = (Nodo *)malloc(sizeof(Nodo));
+    temp->siguiente = NULL;
+    temp->data.dato = dato;
+    return temp;
 }
 
-Nodo *createList()
+head *createHead(int dato)
 {
-    return NULL;
+    head *newHead = (head *)malloc(sizeof(head));
+    newHead->head = createNode(dato);
+    newHead->ultimo = newHead->head;
+    return newHead;
 }
 
-void addNode(Nodo **head, int dato)
+void enqueue(head *cabeza, int dato)
 {
-    Nodo *newNode = createNode(dato);
+    Nodo *nuevoNodo = createNode(dato);
 
-    if (newNode == NULL)
+    if (cabeza->ultimo != NULL)
     {
-        printf("El nodo no pudo reservar la memoria\n");
-        return;
-    }
-
-    newNode->data.dato = dato;
-    newNode->siguiente = NULL;
-
-    if (*head == NULL)
-    {
-        *head = newNode;
-        printf("Nodo añadido correctamente\n");
-        return;
-    }
-
-    Nodo *current = *head;
-    while (current->siguiente != NULL)
-    {
-        current = current->siguiente;
-    }
-
-    current->siguiente = newNode;
-    printf("Nodo añadido correctamente\n");
-}
-
-int pop(Nodo **head)
-{
-    if (*head == NULL)
-    {
-        printf("El nodo está vacío\n");
-        return 0;
-    }
-
-    Nodo *temp = *head;
-    int value = temp->data.dato;
-    *head = (*head)->siguiente;
-    free(temp);
-    return value;
-}
-
-void showList(Nodo **head)
-{
-    if (*head == NULL)
-    {
-        printf("La lista está vacía");
-        return;
+        cabeza->ultimo->siguiente = nuevoNodo;
+        cabeza->ultimo = nuevoNodo;
     }
     else
     {
-        Nodo *tempNode = *head;
-        int i = 1;
-        while (tempNode != NULL)
-        {
-            printf("%d.- %d\n", i, tempNode->data.dato);
-            i++;
-            tempNode = tempNode->siguiente;
-        }
+        cabeza->head = nuevoNodo;
+        cabeza->ultimo = nuevoNodo;
     }
+}
+
+void dequeue(head *cabeza)
+{
+    if (cabeza->head == NULL)
+    {
+        printf("Cola está vacía\n");
+        return;
+    }
+
+    Nodo *temp = cabeza->head;
+    cabeza->head = cabeza->head->siguiente;
+
+    if (cabeza->head == NULL)
+    {
+        cabeza->ultimo = NULL;
+    }
+
+    free(temp);
+}
+
+int getFirstData(head *cabeza)
+{
+    return cabeza->head->data.dato;
+}
+
+Bool isEmpty(head *cabeza)
+{
+    if (cabeza->head == NULL && cabeza->ultimo == NULL)
+    {
+        return True;
+    }
+
+    return False;
+}
+
+int sizeCola(head *cabeza)
+{
+    int i = 0;
+    Nodo *temp = cabeza->head;
+    while (temp != NULL)
+    {
+        i++;
+        temp = temp->siguiente;
+    }
+    return i;
+}
+
+void cleanCola(head *cabeza)
+{
+    while (cabeza->head != NULL)
+    {
+        dequeue(cabeza);
+    }
+    printf("La cola ha sido limpiada exitosamente\n");
+}
+
+void printQueue(head *cabeza)
+{ 
+    printf("Numeros en cola: ");
+    if (isEmpty(cabeza))
+    {
+        printf("la cola esta vacia\n");
+        return;
+    }
+    
+    Nodo *actual = cabeza->head;
+    while (actual != NULL)
+    {
+        printf("%d ", actual->data.dato);
+        actual = actual->siguiente;
+    }
+    printf("\n");
 }
